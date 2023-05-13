b6ff006cf0ad8c682546404b1d03e7eefbf8d96c
Emmanuel <emmanuelbotchwey---@gmail.com>
2023-05-13 04:05:31 -1200

    my js practice

---
+++ myscript.js
@@ -0,0 +1,63 @@
+  //5 string variables
+  let name = 'Emmanuel'
+  let surname = 'Botchwey'
+  let gen = '25'
+  let occupation = 'Student'
+  let gender = 'Male'
+
+  console.log(name)
+  console.log(surname)
+  console.log(gen)
+  console.log(occupation)
+  console.log(gender)
+
+  //5 int variables
+  let int1 = 10
+  let int2 = 20
+  let int3 = 30
+  let int4 = 40
+  let int5 = 50
+
+  console.log(int1)
+  console.log(int2)
+  console.log(int3)
+  console.log(int4)
+  console.log(int5)
+
+  //5 float variables
+  let float1 = 10.1
+  let float2 = 11.2
+  let float3 = 12.3
+  let float4 = 13.4
+  let float5 = 14.5
+
+  console.log(float1)
+  console.log(float2)
+  console.log(float3)
+  console.log(float4)
+  console.log(float5)
+
+  //5 boolean variables 
+  let Dbee = false
+  let Mamaba = true
+  let good = true
+  let bad = false
+  let genius = true
+
+  console.log(Dbee)
+  console.log(Mamaba)
+  console.log(good)
+  console.log(bad)
+  console.log(genius)
+
+  //10 random variables
+  document.write('game')
+  document.write('of')
+  document.write('thrones')
+  document.write('batman')
+  document.write('vampire')
+  document.write('diaries')
+  document.write('blacklist')
+  document.write('redemption')
+  document.write('the')
+  document.write('originals')
